aa596bb622226d87ead84167acb88d0e6cfda2e1
duck2913 <minhducdl1010@gmail.com>
2022-07-23 18:35:59 +0700

    pressing logo to go back home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { ShoppingCart, Aperture } from "tabler-icons-react";
 import { CartContext } from "../context/CartContext";
+import Link from "next/link";
 const Navbar = () => {
 	const { setOpenModal, totalQuantities } = useContext(CartContext);
 
@@ -11,10 +12,12 @@ const Navbar = () => {
 
 	return (
 		<div className="w-[90vw] mx-auto flex justify-between items-center p-3 pt-5 ">
-			<div className="logo font-bold flex items-center gap-2">
-				<Aperture size={48} strokeWidth={2} color={"#d2797a"} />
-				<p className="text-slate-600">Getgo</p>
-			</div>
+			<Link href={"/"} passHref>
+				<div className="logo font-bold flex items-center gap-2">
+					<Aperture size={48} strokeWidth={2} color={"#d2797a"} />
+					<p className="text-slate-600">Getgo</p>
+				</div>
+			</Link>
 			<button className="shopping-cart relative" onClick={openCartModal}>
 				<ShoppingCart size={25} />
 				<div className="badge w-[1.5rem] h-[1.5rem] rounded-[100%] bg-red-500 text-white flex justify-center items-center text-sm absolute top-0 right-0 transform translate-x-[1rem] translate-y-[-10px]">
